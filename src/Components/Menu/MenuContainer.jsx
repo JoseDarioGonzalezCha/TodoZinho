9ac14a529540a677e044f90999dc40e0ae0605f3
unknown <jdariogonzalez.2020@gmail.com>
2023-05-24 14:52:53 -0300

    feat: created StyledSection from styled-componentes

--- src/Components/Menu/MenuContainer.jsx
+++ src/Components/Menu/MenuContainer.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
+import { StyledSection } from "./MenuContainer.styles";
+
 import { MenuHeader } from "./MenuHeader/MenuHeader";
 import { MenuTasks } from "./MenuTasks/MenuTasks";
 import { MenuList } from "./MenuList/MenuList";
 
 export const MenuContainer = () => {
   return (
-    <>
+    <StyledSection>
       <MenuHeader />
       <MenuTasks />
       <MenuList />
-    </>
+    </StyledSection>
   );
 };
---
+++ src/Components/Menu/MenuContainer.styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledSection = styled.section`
+  background-color: #a0a603;
+  width: 30vw;
+`;
