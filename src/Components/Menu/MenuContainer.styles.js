d14601f7a9da51899b1a1b5f744e8f7004f89cf8
unknown <jdariogonzalez.2020@gmail.com>
2023-05-24 15:05:34 -0300

    feat: Stylized component MenuContainer

--- src/Components/Menu/MenuContainer.styles.js
+++ src/Components/Menu/MenuContainer.styles.js
@@ -3,4 +3,7 @@ import styled from "styled-components";
 export const StyledSection = styled.section`
   background-color: #a0a603;
   width: 30vw;
+  height: 100vh;
+  border-radius: 15px;
+  margin: 24px 0 40px 24px;
 `;
