f8cea9cc5ba393df84ac959d5425e083ca037147
unknown <jdariogonzalez.2020@gmail.com>
2023-05-25 09:36:55 -0300

    feat: Created an stylized Component Menu

--- src/Components/Menu/MenuHeader/MenuHeader.jsx
+++ src/Components/Menu/MenuHeader/MenuHeader.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+
+import abacateLendo from "../../../assets/abacateLendo.png";
+
+import { BiGridSmall } from "react-icons/bi";
+import { FiSearch } from "react-icons/fi";
+
+import {
+  StyledContent,
+  StyledImg,
+  StyledInput,
+  StyledMenu,
+  StyledMenuHeader,
+  StyledTitle,
+} from "./MenuHeader.styles";
 
 export const MenuHeader = () => {
   return (
-	<div>Menu Header</div>
-  )
-}
+    <StyledContent>
+      <StyledMenuHeader>
+        <StyledTitle>Menu</StyledTitle>
+        <BiGridSmall />
+      </StyledMenuHeader>
+
+      <StyledImg src={abacateLendo} alt="avocado reading" />
+
+      <StyledMenu>
+        <FiSearch />
+        <StyledInput type="text" placeholder="SEARCH" />
+      </StyledMenu>
+    </StyledContent>
+  );
+};
