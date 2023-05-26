87ca45a163e58cf06d400a5659b95779d1768054
unknown <jdariogonzalez.2020@gmail.com>
2023-05-26 13:42:04 -0300

    feat: creted and stylized component MenuList

--- src/Components/Menu/MenuList/MenuList.jsx
+++ src/Components/Menu/MenuList/MenuList.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from "react";
+
+import personal from "../../../assets/personal.png";
+import school from "../../../assets/school.png";
+import market from "../../../assets/market.png";
+import archiveadd from "../../../assets/archiveadd.png";
+
+import {
+  StyledListContainer,
+  StyledListTasks,
+  StyledListTitle,
+  StyledMarket,
+  StyledNewList,
+  StyledParagraph,
+  StyledPersonal,
+  StyledSchool,
+} from "./MenuList.styles";
 
 export const MenuList = () => {
   return (
-	<div>Menu List</div>
-  )
-}
+    <StyledListContainer>
+      <StyledListTitle>LIST</StyledListTitle>
+      <StyledListTasks>
+        <StyledPersonal>
+          <img src={personal} alt="personal icon" />
+          <StyledParagraph>PERSONAL</StyledParagraph>
+        </StyledPersonal>
+
+        <StyledSchool>
+          <img src={school} alt="school icon" />
+          <StyledParagraph>SCHOOL</StyledParagraph>
+        </StyledSchool>
+
+        <StyledMarket>
+          <img src={market} alt="market icon" />
+          <StyledParagraph>MARKET</StyledParagraph>
+        </StyledMarket>
+
+        <StyledNewList>
+          <img src={archiveadd} alt="new task icon" />
+          <StyledParagraph>ADD NEW LIST</StyledParagraph>
+        </StyledNewList>
+      </StyledListTasks>
+    </StyledListContainer>
+  );
+};
---
+++ src/Components/Menu/MenuList/MenuList.styles.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const StyledListContainer = styled.div`
+  margin: 50px 15px;
+`;
+
+export const StyledListTitle = styled.h3`
+  font-family: "Bebas Neue";
+  font-weight: 400;
+  font-size: 30px;
+  line-height: 30px;
+  text-transform: uppercase;
+  color: #2c4001;
+  margin: 5px 0;
+`;
+
+export const StyledListTasks = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 30px 15px;
+  gap: 24px;
+`;
+
+export const StyledPersonal = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`;
+export const StyledSchool = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`;
+export const StyledMarket = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`;
+export const StyledNewList = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`;
+
+export const StyledParagraph = styled.p`
+  color: #f2f2f2;
+  font-family: "Urbanist";
+  font-weight: 700;
+  font-size: 15px;
+  line-height: 18px;
+`;
