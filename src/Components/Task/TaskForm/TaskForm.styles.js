8b3f863f310e2586a0c5f32ead034df69e29a9c6
unknown <jdariogonzalez.2020@gmail.com>
2023-05-26 14:51:47 -0300

    feat: created TaskForm component

---
+++ src/Components/Task/TaskForm/TaskForm.styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  background-color: blue;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const StyledTitle = styled.h2`
+  font-family: "Bebas Neue", sans-serif;
+  font-weight: 400px;
+  font-size: 40px;
+  text-transform: uppercase;
+  color: #000000;
+`;
