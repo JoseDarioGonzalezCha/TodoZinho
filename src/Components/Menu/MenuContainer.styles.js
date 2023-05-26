4cd12e471e5632cc7faea0fc3319451f99e333b0
unknown <jdariogonzalez.2020@gmail.com>
2023-05-26 13:43:54 -0300

    fix: add height: 100%

--- src/Components/Menu/MenuContainer.styles.js
+++ src/Components/Menu/MenuContainer.styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledSection = styled.section`
   background-color: #a0a603;
   width: 30vw;
-  height: 100vh;
+  height: 100%;
   border-radius: 15px;
   margin: 24px 0 40px 24px;
 `;
