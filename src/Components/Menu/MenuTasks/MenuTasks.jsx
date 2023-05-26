0d32d703246b722756b3f581fefffa95cc35b141
unknown <jdariogonzalez.2020@gmail.com>
2023-05-26 13:42:37 -0300

    feat: creted and stylized component MenuTasks

--- src/Components/Menu/MenuTasks/MenuTasks.jsx
+++ src/Components/Menu/MenuTasks/MenuTasks.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+
+import calendar from "../../../assets/calendar.png";
+import archivetick from "../../../assets/archivetick.png";
+import today from "../../../assets/today.png";
+
+import {
+  StyledAll,
+  StyledCalendar,
+  StyledMenuContainer,
+  StyledMenuTasks,
+  StyledParagraph,
+  StyledTitle,
+  StyledToday,
+} from "./MenuTasks.styles";
 
 export const MenuTasks = () => {
   return (
-	<div>Menu Tasks</div>
-  )
-}
+    <StyledMenuContainer>
+      <StyledTitle>TASKS</StyledTitle>
+
+      <StyledMenuTasks>
+        <StyledCalendar>
+          <img src={calendar} alt="image calendar" />
+          <StyledParagraph>CALENDAR</StyledParagraph>
+        </StyledCalendar>
+
+        <StyledAll>
+          <img src={archivetick} alt="check all" />
+          <StyledParagraph>ALL</StyledParagraph>
+        </StyledAll>
+
+        <StyledToday>
+          <img src={today} alt="image today" />
+          <StyledParagraph>TODAY</StyledParagraph>
+        </StyledToday>
+      </StyledMenuTasks>
+    </StyledMenuContainer>
+  );
+};
