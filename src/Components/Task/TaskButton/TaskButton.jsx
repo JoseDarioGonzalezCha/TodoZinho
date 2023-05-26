7eb005c4e7d521722d1aa033a7d91656c7918fbe
unknown <jdariogonzalez.2020@gmail.com>
2023-05-26 14:51:24 -0300

    feat: created TaskButton component

---
+++ src/Components/Task/TaskButton/TaskButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { StyledButtonContainer } from "./TaskButton.styles";
+
+export const TaskButton = () => {
+  return (
+    <StyledButtonContainer>
+      <button>DELETE TASK</button>
+      <button>SAVE CHANGE</button>
+      TaskButton
+    </StyledButtonContainer>
+  );
+};
