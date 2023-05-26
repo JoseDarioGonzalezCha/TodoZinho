f4b5f24cac5c0db67fef15a86dc88e409ec69d74
unknown <jdariogonzalez.2020@gmail.com>
2023-05-26 14:49:08 -0300

    feat: add TaskContainer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { GlobalStyle } from "./styles/global";
 
 import { MenuContainer } from "./Components/Menu/MenuContainer";
+import { TaskContainer } from "./Components/Task/TaskContainer";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <MenuContainer />
+      <TaskContainer />
     </>
   );
 }
