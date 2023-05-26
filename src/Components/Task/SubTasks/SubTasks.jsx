379bfcc69fc070cf2f53fcfb96788c099589fe7f
unknown <jdariogonzalez.2020@gmail.com>
2023-05-26 14:50:55 -0300

    feat: created SubTask component

---
+++ src/Components/Task/SubTasks/SubTasks.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { StyledSubTaskContainer } from "./SubTAsks.styles";
+
+export const SubTasks = () => {
+  return (
+    <StyledSubTaskContainer>
+      <h2>SUBTASK</h2>
+    </StyledSubTaskContainer>
+  );
+};
