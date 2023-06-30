54c55fa2ec2c7301d8880c20efd31c0bbff830af
Jose Gonzalez <jdariogonzalez.2020@gmail.com>
2023-06-30 17:23:34 -0300

    add styles

--- src/Components/TaskFormContainer/SubTasks/SubTAsks.styles.js
+++ src/Components/TaskFormContainer/SubTasks/SubTAsks.styles.js
@@ -1,5 +1,47 @@
 import styled from "styled-components";
 
-export const StyledSubTaskContainer = styled.div`
-  background-color: yellow;
+export const StyledContainer = styled.h2`
+  margin: 70px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 40px;
+`;
+
+export const StyledH2 = styled.h2`
+  font-family: "Bebas Neue";
+  font-weight: 400px;
+  font-size: 40px;
+  color: #000000;
+`;
+
+export const StyledDiv = styled.div`
+  width: 75%;
+  display: flex;
+  justify-content: flex-start;
+  gap: 30px;
+`;
+
+export const StyledInput = styled.input`
+  border: none;
+  background-color: #d9ce96;
+
+  &::placeholder {
+    font-family: "Urbanist";
+    font-weight: 700px;
+    font-size: 15px;
+  }
+`;
+
+export const StyledCheck = styled.input`
+  background-color: #f2f2f2;
+  width: 40px;
+  border: 3px solid gray;
+`;
+
+export const StyledParagraphe = styled.p`
+  font-family: "Urbanist";
+  font-weight: 700px;
+  font-size: 15px;
+  color: #515249;
 `;
--- src/Components/TaskFormContainer/SubTasks/SubTasks.jsx
+++ src/Components/TaskFormContainer/SubTasks/SubTasks.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 
-import { StyledSubTaskContainer } from "./SubTAsks.styles";
+import Icon from "../../../assets/vector2.png";
+
+import {
+  StyledCheck,
+  StyledContainer,
+  StyledDiv,
+  StyledH2,
+  StyledInput,
+  StyledParagraphe,
+} from "./SubTAsks.styles";
 
 export const SubTasks = () => {
   return (
-    <StyledSubTaskContainer>
-      <h2>SUBTASK</h2>
-    </StyledSubTaskContainer>
+    <StyledContainer>
+      <StyledH2>SUBTASK:</StyledH2>
+      <StyledDiv>
+        <img src={Icon} alt="vector2" />
+        <StyledInput type="text" placeholder="ADD NEW SUBTASK" />
+      </StyledDiv>
+      <StyledDiv>
+        <StyledCheck type="checkbox" />
+        <StyledParagraphe>SUBTASK</StyledParagraphe>
+      </StyledDiv>
+    </StyledContainer>
   );
 };
